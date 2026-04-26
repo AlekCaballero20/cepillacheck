23d2b685c36565945c420e67fd553ac0099d51ea
AlekCaballero20 <alekcaballeromusic@gmail.com>
2026-04-26 09:05:29 -0500

    Add files via upload

--- js/core/state.js
+++ js/core/state.js
@@ -1,7 +1,25 @@
 export const state = {
   currentUser: null,
-  todayData: { manana: false, noche: false },
-  partnerData: { manana: false, noche: false },
+  todayData: {
+    manana: false,
+    manana_seda: false,
+    manana_enjuague: false,
+    manana_irrigador: false,
+    noche: false,
+    noche_seda: false,
+    noche_enjuague: false,
+    noche_irrigador: false,
+  },
+  partnerData: {
+    manana: false,
+    manana_seda: false,
+    manana_enjuague: false,
+    manana_irrigador: false,
+    noche: false,
+    noche_seda: false,
+    noche_enjuague: false,
+    noche_irrigador: false,
+  },
   currentScreen: '',
 };
 
@@ -12,14 +30,26 @@ export function setCurrentUser(user) {
 export function setTodayData(data = {}) {
   state.todayData = {
     manana: Boolean(data.manana),
+    manana_seda: Boolean(data.manana_seda),
+    manana_enjuague: Boolean(data.manana_enjuague),
+    manana_irrigador: Boolean(data.manana_irrigador),
     noche: Boolean(data.noche),
+    noche_seda: Boolean(data.noche_seda),
+    noche_enjuague: Boolean(data.noche_enjuague),
+    noche_irrigador: Boolean(data.noche_irrigador),
   };
 }
 
 export function setPartnerData(data = {}) {
   state.partnerData = {
     manana: Boolean(data.manana),
+    manana_seda: Boolean(data.manana_seda),
+    manana_enjuague: Boolean(data.manana_enjuague),
+    manana_irrigador: Boolean(data.manana_irrigador),
     noche: Boolean(data.noche),
+    noche_seda: Boolean(data.noche_seda),
+    noche_enjuague: Boolean(data.noche_enjuague),
+    noche_irrigador: Boolean(data.noche_irrigador),
   };
 }
 
